fe2f20e224e63212065bcc9a5bca49acbc0b046c
azirariza9 <azirariza9@gmail.com>
2025-06-15 10:11:25 +0700

    add KelasMakna controller

---
+++ src/main/java/com/azirariza/javadict/controller/KelasMaknaController.java
@@ -0,0 +1,65 @@
+package com.azirariza.javadict.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.azirariza.javadict.entity.KelasMakna;
+import com.azirariza.javadict.entity.dto.KelasMaknaDTOInsert;
+import com.azirariza.javadict.entity.dto.KelasMaknaDTOUpdate;
+import com.azirariza.javadict.service.KelasMaknaService;
+
+@RestController
+@RequestMapping({ "/api/kelas_makna", "api/kelas_makna/" })
+public class KelasMaknaController {
+    private final KelasMaknaService kelasMaknaService;
+
+    public KelasMaknaController(KelasMaknaService kelasMaknaService) {
+        this.kelasMaknaService = kelasMaknaService;
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "API is working!";
+    }
+
+    @GetMapping
+    public ResponseEntity<List<KelasMakna>> getAllEntri() {
+        return ResponseEntity.ok(kelasMaknaService.getAllKelasMakna());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<KelasMakna> getEntriById(@PathVariable int id) {
+        Optional<KelasMakna> entri = kelasMaknaService.getKelasMaknaById(id);
+        return entri.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<KelasMakna> createEntri(@RequestBody KelasMaknaDTOInsert kelasMaknaDTO) {
+        KelasMakna createdKata = kelasMaknaService.createKelasMakna(kelasMaknaDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdKata);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<KelasMakna> updateEntri(@PathVariable int idKelasMakna,
+            @RequestBody KelasMaknaDTOUpdate kelasMaknaDTO) {
+        return ResponseEntity.ok(kelasMaknaService.updateKelasMakna(idKelasMakna, kelasMaknaDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEntri(@PathVariable int idKelasMakna) {
+        kelasMaknaService.deleteKelasMakna(idKelasMakna);
+        return ResponseEntity.noContent().build();
+    }
+}
