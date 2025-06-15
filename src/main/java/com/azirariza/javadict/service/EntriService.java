0ef8175b1ac7cee1914fd8c575a71b338c477175
azirariza9 <azirariza9@gmail.com>
2025-06-15 07:20:41 +0700

    add Entri service

---
+++ src/main/java/com/azirariza/javadict/service/EntriService.java
@@ -0,0 +1,42 @@
+package com.azirariza.javadict.service;
+
+import org.springframework.stereotype.Service;
+
+import com.azirariza.javadict.repository.EntriRepository;
+import com.azirariza.javadict.entity.Entri;
+import com.azirariza.javadict.entity.dto.EntriDTOUpdate;
+import com.azirariza.javadict.entity.dto.EntriDTOInsert;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EntriService {
+        private final EntriRepository entriRepository;
+
+    public EntriService(EntriRepository entriRepository) {
+        this.entriRepository = entriRepository;
+    }
+
+    public List<Entri> getAllEntri() {
+        return entriRepository.findAll();
+    }
+
+    public Optional<Entri> getEntriById(String id) {
+        return entriRepository.findById(id);
+    }
+
+    public Entri createEntri(EntriDTOInsert entriDTO) {
+        return entriRepository.insert(entriDTO);
+    }
+
+    public Entri updateEntri(int idEntri, EntriDTOUpdate entriDTO) { 
+        return entriRepository.update(idEntri,entriDTO);
+    }
+
+    public void deleteEntri(String id) {
+        entriRepository.deleteById(id);
+    }
+
+}
