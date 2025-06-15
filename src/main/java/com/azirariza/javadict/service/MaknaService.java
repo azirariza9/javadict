c1d4749e1fc034b39958b8f35bfde426caae9300
azirariza9 <azirariza9@gmail.com>
2025-06-15 16:06:47 +0700

    add Makna service

---
+++ src/main/java/com/azirariza/javadict/service/MaknaService.java
@@ -0,0 +1,41 @@
+package com.azirariza.javadict.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.azirariza.javadict.entity.Makna;
+import com.azirariza.javadict.entity.dto.MaknaDTOInsert;
+import com.azirariza.javadict.entity.dto.MaknaDTOUpdate;
+import com.azirariza.javadict.repository.MaknaRepository;
+
+@Service
+public class MaknaService {
+    private final MaknaRepository maknaRepository;
+
+    public MaknaService(MaknaRepository maknaRepository) {
+        this.maknaRepository = maknaRepository;
+    }
+
+    public List<Makna> getAllMakna() {
+        return maknaRepository.findAll();
+    }
+
+    public Optional<Makna> getMaknaById(int id) {
+        return maknaRepository.findById(id);
+    }
+
+    public Makna createMakna(MaknaDTOInsert maknaDTO) {
+        return maknaRepository.insert(maknaDTO);
+    }
+
+    public Makna updateMakna(int idMakna, MaknaDTOUpdate maknaDTO) {
+        return maknaRepository.update(idMakna, maknaDTO);
+    }
+
+    public void deleteMakna(int idMakna) {
+        maknaRepository.deleteById(idMakna);
+    }
+
+}
